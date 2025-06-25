6038c7671be35b65f70cf073111ae0be3a64cea3
Fife <fife.team005@gridirontest.com>
2025-06-25 15:22:33 +0100

    Adding remote backend func for monitoring terraform state across workflow jobs

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "the-devops-man-tfstate"
+    key            = "dev/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
 }
 
 # Configure the AWS Provider
